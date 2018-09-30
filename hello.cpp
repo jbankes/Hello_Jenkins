7c370564accd2d803bef5bd133f339fe882fd4ab
Sanyam Dogra <33497630+sanyamdogra@users.noreply.github.com>
2018-10-01 00:16:18 +0530

    New line

--- hello.cpp
+++ hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-
+  cout<<"Hello,welcome to the world Jenkins\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
