4fb95363ffd900f1be8044191f01e2cbba157352
Koushik gopi krishnan <koushikgopikrishnan@gmail.com>
2020-01-23 14:08:53 +0530

    added comments
    just added a comment

--- original/hello.cpp
+++ original/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
+  cout << "Hello, Jenkins\n";   //for printing 
 
   cout << "I have successfully built and run\n";
   return 0;
